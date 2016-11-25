da88ca357d0d8d804f8c5760d2feaa6da682f7a7
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-11-25 12:50:32 +0100

    add Tokenize utils function

---
+++ src/Bayes/Utils/Text.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Bayes.Utils
+{
+    public static class Text
+    {
+        public static IEnumerable<string> Tokenize(this string word)
+        {
+            return word.Split(' ').Select(x => Regex.Replace(x, @"\W", "").ToLower());
+        }
+    }
+}
