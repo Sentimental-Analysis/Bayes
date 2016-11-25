d960176e9052f38186c0d166f30c7a9b9be8cd5e
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-11-25 12:22:57 +0100

    add Predicator

---
+++ src/Bayes/Predicators/Implementation/Predicator.cs
@@ -0,0 +1,13 @@
+﻿using Bayes.Data;
+using Bayes.Predicators.Interfaces;
+
+namespace Bayes.Predicators.Implementation
+{
+    public class Predicator : ITweetPredicator
+    {
+        public Score Predict(string source)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
