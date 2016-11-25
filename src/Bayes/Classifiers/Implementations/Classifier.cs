56c4d16f917f4f42725a0d6b1d81ea9aad118a19
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-11-25 12:22:21 +0100

    add Classifier

---
+++ src/Bayes/Classifiers/Implementations/Classifier.cs
@@ -0,0 +1,13 @@
+﻿using Bayes.Classifiers.Interfaces;
+using Bayes.Data;
+
+namespace Bayes.Classifiers.Implementations
+{
+    public class Classifier : ITweetClassifier
+    {
+        public string Classify(Analysis source)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
