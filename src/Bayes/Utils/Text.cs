a79191a6d4589bc747ac6a658097dcb205cb4b27
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-26 18:38:41 +0100

    add Frequency function

--- src/Bayes/Utils/Text.cs
+++ src/Bayes/Utils/Text.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -9,6 +10,19 @@ namespace Bayes.Utils
         public static IEnumerable<string> Tokenize(this string word)
         {
             return word.Split(' ').Select(x => Regex.Replace(x, @"\W", "").ToLower());
+        }
+
+        public static ImmutableDictionary<string, int> Frequency(this IEnumerable<string> words)
+        {
+            return words.Aggregate(ImmutableDictionary<string, int>.Empty, (acc, x) =>
+            {
+                int freq;
+                if (acc.TryGetValue(x, out freq))
+                {
+                    return acc.SetItem(x, freq + 1);
+                }
+                return acc.Add(x, 1);
+            });
         }
     }
 }
