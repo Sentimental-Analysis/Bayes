4af2953680c57c6ab36db0d5ad9ff2f33b4f24f7
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-27 17:16:16 +0100

    add Learn method to classifier

--- src/Bayes/Classifiers/Interfaces/IClassifier.cs
+++ src/Bayes/Classifiers/Interfaces/IClassifier.cs
@@ -1,7 +1,10 @@
-﻿namespace Bayes.Classifiers.Interfaces
+﻿using Bayes.Data;
+
+namespace Bayes.Classifiers.Interfaces
 {
     public interface IClassifier<in TIn, out TOut>
     {
         TOut Classify(TIn source);
+        void Learn(Category category, TIn source);
     }
 }
