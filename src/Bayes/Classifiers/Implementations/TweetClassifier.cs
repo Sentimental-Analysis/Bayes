ac06925c59050c6a3b00827e51793101ce7dfbb8
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-27 16:48:44 +0100

    add TweetClassifier

---
+++ src/Bayes/Classifiers/Implementations/TweetClassifier.cs
@@ -0,0 +1,13 @@
+﻿using Bayes.Classifiers.Interfaces;
+using Bayes.Data;
+
+namespace Bayes.Classifiers.Implementations
+{
+    public class TweetClassifier : IClassifier<string, Score>
+    {
+        public Score Classify(string source)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
