e68c002a3ef9812a4ffd97656f1dd2e468179824
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-27 17:13:50 +0100

    add Classification class

--- src/Bayes/Data/Classification.cs
+++ src/Bayes/Data/Classification.cs
@@ -4,8 +4,22 @@ namespace Bayes.Data
 {
     public class Classification
     {
-        public Probability Probability { get; set; }
-        public Category Category { get; set; }
-        public IEnumerable<string> Words { get; set; }
+        public Probability Probability { get; }
+        public Category Category { get; }
+        public IEnumerable<string> Words { get; }
+
+        public Classification(Probability probability, Category category, IEnumerable<string> words)
+        {
+            Probability = probability;
+            Category = category;
+            Words = words;
+        }
+
+        public Classification(Category category, IEnumerable<string> words)
+        {
+            Probability = 1.0;
+            Category = category;
+            Words = words;
+        }
     }
 }
