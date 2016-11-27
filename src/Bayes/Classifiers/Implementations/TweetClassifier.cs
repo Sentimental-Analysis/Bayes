83ced40a73b5e418b4ba6921201d9bbcf85935ed
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-27 20:11:54 +0100

    small changes

--- src/Bayes/Classifiers/Implementations/TweetClassifier.cs
+++ src/Bayes/Classifiers/Implementations/TweetClassifier.cs
@@ -5,6 +5,14 @@ namespace Bayes.Classifiers.Implementations
 {
     public class TweetClassifier : IClassifier<string, Score>
     {
+        private readonly AnalysisResult _analysisResult;
+
+        public TweetClassifier(AnalysisResult analysisResult)
+        {
+            _analysisResult = analysisResult;
+        }
+
+
         public Score Classify(string source)
         {
             throw new System.NotImplementedException();
