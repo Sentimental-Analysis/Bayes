aa637ba2a8a0c645d6e37ca9b22b341807588594
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-27 19:44:48 +0100

    add test for increment category method

---
+++ test/Bayes.Tests/Model/AnalysisResultTests.cs
@@ -0,0 +1,37 @@
+﻿using Bayes.Data;
+using FluentAssertions;
+using Xunit;
+
+namespace Bayes.Tests.Model
+{
+    public class AnalysisResultTests
+    {
+        [Theory]
+        [InlineData(Category.Negative)]
+        [InlineData(Category.Positive)]
+        public void Test_Increment_Category_Method_When_No_Exist_In_Dict(Category testCategory)
+        {
+            var analysisResult = AnalysisResult.Empty();
+            var testResult = analysisResult.IncrementCategory(testCategory);
+            int count;
+            if (testResult.TotalCategory.TryGetValue(testCategory, out count))
+            {
+                count.Should().Be(1);
+            }
+        }
+
+        [Theory]
+        [InlineData(Category.Negative)]
+        [InlineData(Category.Positive)]
+        public void Test_Increment_Category_Method_When_Exist_In_Dict(Category testCategory)
+        {
+            var analysisResult = AnalysisResult.Empty();
+            var testResult = analysisResult.IncrementCategory(testCategory).IncrementCategory(testCategory);
+            int count;
+            if (testResult.TotalCategory.TryGetValue(testCategory, out count))
+            {
+                count.Should().Be(2);
+            }
+        }
+    }
+}
