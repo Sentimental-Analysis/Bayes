539831ab473cfea3e9231a9a58ea23c8cfe83ebb
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-11-28 15:46:46 +0100

    small changes

--- src/Bayes/Classifiers/Implementations/TweetClassifier.cs
+++ src/Bayes/Classifiers/Implementations/TweetClassifier.cs
@@ -1,13 +1,36 @@
-﻿using Bayes.Classifiers.Interfaces;
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using Bayes.Classifiers.Interfaces;
 using Bayes.Data;
+using Bayes.Utils;
+using System.Linq;
 
 namespace Bayes.Classifiers.Implementations
 {
     public class TweetClassifier : IClassifier<Score, string>
     {
+        private readonly LearnerState _learnerState;
+
+        public TweetClassifier(LearnerState learnerState)
+        {
+            _learnerState = learnerState;
+        }
+
         public Score Classify(string parameter)
         {
-            throw new System.NotImplementedException();
+            var words = parameter.Tokenize();
+            var aprioriProbability = GetAprioriProbability();
+            return null;
+        }
+
+        public ImmutableDictionary<WordCategory, Probability> GetAprioriProbability()
+        {
+            int allElements = _learnerState.CategoryPerQuantity.Select(x => x.Value).Sum();
+            return _learnerState.CategoryPerQuantity.Keys.Select(key =>
+            {
+                int count = _learnerState.CategoryPerQuantity.GetValueOrDefault(key);
+                return new KeyValuePair<WordCategory, Probability>(key, new Probability((double)count / allElements));
+            }).ToImmutableDictionary();
         }
     }
 }
--- src/Bayes/Data/Probability.cs
+++ src/Bayes/Data/Probability.cs
@@ -12,5 +12,10 @@
         public static implicit operator double(Probability probability) => probability.Value;
         public static implicit operator Probability(double value) => new Probability(value);
 
+        public static Probability operator *(Probability probability1, Probability probability2)
+        {
+            return new Probability(probability1.Value * probability2.Value);
+        }
+
     }
 }
