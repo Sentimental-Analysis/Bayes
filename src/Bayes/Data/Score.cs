76354df13d6d7015f37175934a3513d90ec30069
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-11-28 15:12:19 +0100

    add Score

---
+++ src/Bayes/Data/Score.cs
@@ -0,0 +1,15 @@
+﻿namespace Bayes.Data
+{
+    public class Score
+    {
+        public Sentence Sentence { get; }
+
+        public Probability Probability { get; }
+
+        public Score(Sentence sentence, Probability probability)
+        {
+            Sentence = sentence;
+            Probability = probability;
+        }
+    }
+}
