3dd0fb61ed430c424c481ae64bc755fe9cfc89f5
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-11-30 11:21:31 +0100

    small chnages

--- src/Bayes/Classifiers/Implementations/TweetClassifier.cs
+++ src/Bayes/Classifiers/Implementations/TweetClassifier.cs
@@ -19,17 +19,39 @@ namespace Bayes.Classifiers.Implementations
         public Score Classify(string parameter)
         {
             var words = parameter.Tokenize();
+            int allElementsQuantity = _learnerState.CategoryPerQuantity.Select(x => x.Value).Sum();
             var aprioriProbability = GetAprioriProbability();
             return null;
         }
 
+
+        public ImmutableDictionary<WordCategory, Probability> GetPrioriProbability(IEnumerable<string> words, int quantity)
+        {
+            var res = words.Select(word => GetPrioriProbabilityForSingleWord(word, quantity));
+            return null;
+        }
+        public IEnumerable<KeyValuePair<WordCategory, Probability>> GetPrioriProbabilityForSingleWord(string word, int quantity)
+        {
+            var categories = _learnerState.CategoryPerQuantity.Keys;
+            return categories.Select(category =>
+            {
+                ImmutableDictionary<string, int> wordPerQuantity;
+                if (_learnerState.CategoryPerWords.TryGetValue(category, out wordPerQuantity))
+                {
+                    int count = wordPerQuantity.GetValueOrDefault(word);
+                    return new KeyValuePair<WordCategory, Probability>(category, new Probability((double)count / quantity));
+                }
+                return new KeyValuePair<WordCategory, Probability>(category, 0.0);
+            });
+        }
+
         public ImmutableDictionary<WordCategory, Probability> GetAprioriProbability()
         {
-            int allElements = _learnerState.CategoryPerQuantity.Select(x => x.Value).Sum();
+            int allElementsQuantity = _learnerState.CategoryPerQuantity.Select(x => x.Value).Sum();
             return _learnerState.CategoryPerQuantity.Keys.Select(key =>
             {
                 int count = _learnerState.CategoryPerQuantity.GetValueOrDefault(key);
-                return new KeyValuePair<WordCategory, Probability>(key, new Probability((double)count / allElements));
+                return new KeyValuePair<WordCategory, Probability>(key, new Probability((double)count / allElementsQuantity));
             }).ToImmutableDictionary();
         }
     }
