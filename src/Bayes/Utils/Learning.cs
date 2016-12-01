2dd1cb201010ac527df0b19de94fe3eaa48006eb
Dominik Kotecki <dominikus1910@gmail.com>
2016-12-01 20:43:32 +0100

    add Learning utils class

---
+++ src/Bayes/Utils/Learning.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Bayes.Data;
+using Bayes.Learner.Implementations;
+
+namespace Bayes.Utils
+{
+    public static class Learning
+    {
+        public static LearnerState FromDictionary(ImmutableDictionary<string, int> source)
+        {
+            var learner = new TweetLearner();
+            return source.Aggregate(LearnerState.Empty, (acc, x) => learner.Learn(acc,
+                new Sentence(x.Key, x.Value >= 0 ? WordCategory.Positive : WordCategory.Negative)));
+        }
+    }
+}
