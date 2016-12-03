61c300022a045963727479b2fe535e89ec8c0efe
Dominik Kotecki <dominikus1910@gmail.com>
2016-12-03 18:41:17 +0100

    Test_Tweet_Traine

---
+++ test/Bayes.Tests/Trainers/TweetTrainerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+using Bayes.Data;
+using Bayes.Learner.Implementations;
+using Bayes.Trainers.Implementations;
+using FluentAssertions;
+using Xunit;
+
+namespace Bayes.Tests.Trainers
+{
+    public class TweetTrainerTests
+    {
+        [Fact]
+        public void Test_Tweet_Traine()
+        {
+            var trainer = new TweetTrainer(new TweetLearner());
+            trainer.Train(new Sentence("test", WordCategory.Positive));
+            trainer.Train(new Sentence("test", WordCategory.Negative));
+            trainer.CurrentState.CategoryPerQuantity.Keys.Should().NotBeEmpty();
+            trainer.CurrentState.CategoryPerQuantity.Keys.Should().Contain(WordCategory.Positive).And.Contain(WordCategory.Negative);
+            trainer.CurrentState.WordPerQuantity.Keys.Should().NotBeEmpty();
+            trainer.CurrentState.WordPerQuantity.Keys.Should().Contain("test");
+            trainer.CurrentState.WordPerQuantity.GetValueOrDefault("test").Should().Be(2);
+        }
+    }
+}
