0599bc761934f8fceb86fde12641eca43e9c7479
Dominik Kotecki <dominikus1910@gmail.com>
2016-12-03 18:48:31 +0100

    make Trainer thread safety

--- src/Bayes/Trainers/Implementations/TweetTrainer.cs
+++ src/Bayes/Trainers/Implementations/TweetTrainer.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Bayes.Data;
 using Bayes.Learner.Interfaces;
 using Bayes.Trainers.Interfaces;
@@ -24,12 +26,38 @@ namespace Bayes.Trainers.Implementations
 
         public void Train(Sentence trainData)
         {
-            CurrentState = _learner.Learn(CurrentState, trainData);
+            var state = CurrentState;
+            bool isLocked = false;
+            try
+            {
+                Monitor.Enter(state, ref isLocked);
+                CurrentState = _learner.Learn(CurrentState, trainData);
+            }
+            finally
+            {
+                if (isLocked)
+                {
+                    Monitor.Exit(state);
+                }
+            }
         }
 
         public void Train(IEnumerable<Sentence> trainDataArray)
         {
-            CurrentState = trainDataArray.Aggregate(CurrentState, (acc, x) => _learner.Learn(acc, x));
+            var state = CurrentState;
+            bool isLocked = false;
+            try
+            {
+                Monitor.Enter(state, ref isLocked);
+                CurrentState = trainDataArray.Aggregate(CurrentState, (acc, x) => _learner.Learn(acc, x));
+            }
+            finally
+            {
+                if (isLocked)
+                {
+                    Monitor.Exit(state);
+                }
+            }
         }
 
         public LearnerState CurrentState { get; private set; }
