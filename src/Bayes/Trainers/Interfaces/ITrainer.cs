e12a7b3ebf329b6506f88e1a2aec7506ad4f7a32
Dominik Kotecki <dominikus1910@gmail.com>
2016-12-03 17:28:04 +0100

    add ITrainer

--- src/Bayes/Trainers/Interfaces/ITrainer.cs
+++ src/Bayes/Trainers/Interfaces/ITrainer.cs
@@ -1,7 +1,11 @@
-﻿namespace Bayes.Trainers.Interfaces
-{
-    public interface ITrainer
-    {
+﻿using System.Collections.Generic;
 
+namespace Bayes.Trainers.Interfaces
+{
+    public interface ITrainer<out TState, in TTrain>
+    {
+        void Train(TTrain trainData);
+        void Train(IEnumerable<TTrain> trainDataArray);
+        TState CurrentState { get; }
     }
 }
